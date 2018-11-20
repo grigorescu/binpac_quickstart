d862ec400a8324ea77a12df0d9291dfa1e9e0ef7
Michael Dopheide <dopheide@es.net>
2018-11-20 13:58:59 -0600

    Allow user to specify an optional namepsace for the plugin.

--- start.py
+++ start.py
@@ -6,7 +6,9 @@ Usage:
 
 
 Arguments:
-    NAME                 - Short name of protocol to be used in filenames (e.g. HTTP)
+    NAME                 - Short name of protocol to be used in filenames (e.g. HTTP).  You may optionally
+                           include a plugin namespace, eg "Space::NAME"  The default namespace is otherwise
+                           just "Bro".
     DESCRIPTION          - Long name of protocol (e.g. Hypertext Transfer Protocol)
     PATH_TO_BRO_SRC      - Full path to the Bro source directory, where the files will be written.
                              e.g. ~/src/bro-2.2/
@@ -19,7 +21,7 @@ Options:
     --buffered           - Enable the flow buffer, enabling use of &oneline and &length
                            in record types. Without this option, it will be a datagram analyzer,
                            which is faster but has no incremental input or buffering support.
-   --plugin              - Create the BinPac files as a plugin. The path to the plugin is substituted for
+   --plugin  - Create the BinPac files as a plugin. The path to the plugin is substituted for
                            the Bro source directory (PATH_TO_BRO_SRC).
 """
 
@@ -44,13 +46,21 @@ def mkdir(path):
         raise e
 
 def main(arguments):
+    # Split the optional namespace out of NAME
+    if "::" in arguments['NAME']:
+        (namespace,name) = arguments['NAME'].split("::",1)
+    else:
+        name = arguments['NAME']
+        namespace = "Bro"
+
+
     if arguments['--plugin']:
         pac_path = os.path.join(arguments['PATH_TO_BRO_SRC'], "src")
         script_path = os.path.join(arguments['PATH_TO_BRO_SRC'], "scripts")
         do_plugin = True
     else:
-        pac_path = os.path.join(arguments['PATH_TO_BRO_SRC'], "src/analyzer/protocol", arguments['NAME'].lower())
-        script_path = os.path.join(arguments['PATH_TO_BRO_SRC'], "scripts/base/protocols", arguments['NAME'].lower())
+        pac_path = os.path.join(arguments['PATH_TO_BRO_SRC'], "src/analyzer/protocol", name.lower())
+        script_path = os.path.join(arguments['PATH_TO_BRO_SRC'], "scripts/base/protocols", name.lower())
         do_plugin = False
         mkdir(pac_path)
         mkdir(script_path)
@@ -67,28 +77,28 @@ def main(arguments):
         fout = open(os.path.join(arguments['PATH_TO_BRO_SRC'], "CMakeLists.txt"), 'w')
     else:
         fout = open(os.path.join(pac_path, "CMakeLists.txt"), 'w')
-    fout.write(template.render(name=arguments['NAME']))
+    fout.write(template.render(name=name,space=namespace))
     fout.close()
 
     fin = open("./templates/plugin_cc.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
     fout = open(os.path.join(pac_path, "Plugin.cc"), 'w')
-    fout.write(template.render(name=arguments['NAME'], desc=arguments['DESCRIPTION']))
+    fout.write(template.render(name=name, desc=arguments['DESCRIPTION'], space=namespace))
     fout.close()
 
     fin = open("./templates/protocol_cc.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
-    fout = open(os.path.join(pac_path, "%s.cc" % arguments['NAME'].upper()), 'w')
-    fout.write(template.render(name=arguments['NAME'], tcp=arguments['--tcp'], udp=arguments['--udp']))
+    fout = open(os.path.join(pac_path, "%s.cc" % name.upper()), 'w')
+    fout.write(template.render(name=name, tcp=arguments['--tcp'], udp=arguments['--udp']))
     fout.close()
 
     fin = open("./templates/protocol_h.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
-    fout = open(os.path.join(pac_path, "%s.h" % arguments['NAME'].upper()), 'w')
-    fout.write(template.render(name=arguments['NAME'], tcp=arguments['--tcp'], udp=arguments['--udp']))
+    fout = open(os.path.join(pac_path, "%s.h" % name.upper()), 'w')
+    fout.write(template.render(name=name, tcp=arguments['--tcp'], udp=arguments['--udp']))
     fout.close()
 
     # 2. Events
@@ -97,7 +107,7 @@ def main(arguments):
     template = Template(fin.read())
     fin.close()
     fout = open(os.path.join(pac_path, "events.bif"), 'w')
-    fout.write(template.render(name=arguments['NAME']))
+    fout.write(template.render(name=name))
     fout.close()
 
     # 3. PAC files
@@ -105,22 +115,22 @@ def main(arguments):
     fin = open("./templates/protocol_pac.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
-    fout = open(os.path.join(pac_path, "%s.pac" % arguments['NAME'].lower()), 'w')
-    fout.write(template.render(name=arguments['NAME'], desc=arguments['DESCRIPTION']))
+    fout = open(os.path.join(pac_path, "%s.pac" % name.lower()), 'w')
+    fout.write(template.render(name=name, desc=arguments['DESCRIPTION']))
     fout.close()
 
     fin = open("./templates/protocol-protocol_pac.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
-    fout = open(os.path.join(pac_path, "%s-protocol.pac" % arguments['NAME'].lower()), 'w')
-    fout.write(template.render(name=arguments['NAME']))
+    fout = open(os.path.join(pac_path, "%s-protocol.pac" % name.lower()), 'w')
+    fout.write(template.render(name=name))
     fout.close()
 
     fin = open("./templates/protocol-analyzer_pac.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
-    fout = open(os.path.join(pac_path, "%s-analyzer.pac" % arguments['NAME'].lower()), 'w')
-    fout.write(template.render(name=arguments['NAME']))
+    fout = open(os.path.join(pac_path, "%s-analyzer.pac" % name.lower()), 'w')
+    fout.write(template.render(name=name))
     fout.close()
 
     # 4. Scripts
@@ -129,21 +139,21 @@ def main(arguments):
     template = Template(fin.read())
     fin.close()
     fout = open(os.path.join(script_path, "__load__.bro"), 'w')
-    fout.write(template.render(name=arguments['NAME']))
+    fout.write(template.render(name=name))
     fout.close()
 
     fin = open("./templates/dpd_sig.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
     fout = open(os.path.join(script_path, "dpd.sig"), 'w')
-    fout.write(template.render(name=arguments['NAME'], tcp=arguments['--tcp'], udp=arguments['--udp']))
+    fout.write(template.render(name=name, tcp=arguments['--tcp'], udp=arguments['--udp']))
     fout.close()
 
     fin = open("./templates/main_bro.jinja2", 'r')
     template = Template(fin.read())
     fin.close()
     fout = open(os.path.join(script_path, "main.bro"), 'w')
-    fout.write(template.render(name=arguments['NAME'], tcp=arguments['--tcp'], udp=arguments['--udp']))
+    fout.write(template.render(name=name, tcp=arguments['--tcp'], udp=arguments['--udp']))
     fout.close()
 
     # 5. Add it to protocol/CMakeLIsts.txt
@@ -152,7 +162,7 @@ def main(arguments):
         fin = open(os.path.join(arguments['PATH_TO_BRO_SRC'], "src/analyzer/protocol", "CMakeLists.txt"), 'r')
         protocols = fin.readlines()
         fin.close()
-        protocols.append("add_subdirectory(%s)\n" % arguments['NAME'].lower())
+        protocols.append("add_subdirectory(%s)\n" % name.lower())
         protocols.sort()
         fout = open(os.path.join(arguments['PATH_TO_BRO_SRC'], "src/analyzer/protocol", "CMakeLists.txt"), 'w')
         fout.writelines(protocols)
@@ -162,7 +172,7 @@ def main(arguments):
         fin = open(os.path.join(arguments['PATH_TO_BRO_SRC'], "scripts/base", "init-default.bro"), 'r')
         init_default = fin.readlines()
         fin.close()
-        load_cmd = "@load base/protocols/%s\n" % arguments['NAME'].lower()
+        load_cmd = "@load base/protocols/%s\n" % name.lower()
         for i in range(len(init_default)-1):
             if "@load base/protocols/" in init_default[i]:
                 if load_cmd > init_default[i] and (load_cmd < init_default[i+1] or init_default[i+1] == '\n'):
