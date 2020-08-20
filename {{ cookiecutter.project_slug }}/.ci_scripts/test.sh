9ff77fc51545ba33e63fa6f8fd2145d52de83745
Vlad Grigorescu <vlad@es.net>
2020-08-20 11:52:30 -0500

    Include the build log on errors

--- {{ cookiecutter.project_slug }}/.ci_scripts/test.sh
+++ {{ cookiecutter.project_slug }}/.ci_scripts/test.sh
@@ -3,19 +3,25 @@
 set -e
 
 function debug_and_die {
-    OUTPUT_PATH=$HOME/.zkg/testing/external_dns/clones/external_dns
+    OUTPUT_PATH=$HOME/.zkg/testing/{{ cookiecutter.project_slug }}/clones/{{ cookiecutter.project_slug }}
     if [ -s $OUTPUT_PATH/zkg.test_command.stdout ]; then
 	echo "zkg test command stdout"
 	echo "-----------------------"
 	cat $OUTPUT_PATH/zkg.test_command.stdout
     fi
-    
+
     if [ -s $OUTPUT_PATH/zkg.test_command.stderr ]; then
 	echo "zkg test command stderr"
 	echo "-----------------------"
 	cat $OUTPUT_PATH/zkg.test_command.stderr
     fi
-    
+
+    if [ -s $HOME/.zkg/logs/{{ cookiecutter.project_slug }}-build.log ]; then
+	echo "zkg build command output"
+	echo "-----------------------"
+	cat $HOME/.zkg/logs/{{ cookiecutter.project_slug }}-build.log
+    fi
+
     exit 1
 }
 
