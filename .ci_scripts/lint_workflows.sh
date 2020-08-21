911387cf4fca499636a0987f089d1738cfe5145a
Vlad Grigorescu <vlad@es.net>
2020-08-20 21:17:27 -0500

    Ok, fix CI scripts for real

--- .ci_scripts/lint_workflows.sh
+++ .ci_scripts/lint_workflows.sh
@@ -4,7 +4,7 @@ set -e
 
 curl -o /tmp/github-workflow.json https://raw.githubusercontent.com/Logerfo/schemastore/master/src/schemas/json/github-workflow.json
 
-for i in */.github/workflows/*.yml
+for i in *_analyzer/.github/workflows/*.yml
 do
     name=$(basename "$i")
     cat $i | python3 -c 'import json, sys; from ruamel import yaml ; y=yaml.safe_load(sys.stdin.read()) ; json.dump(y, sys.stdout)' > /tmp/workflow-lint-$name.json
